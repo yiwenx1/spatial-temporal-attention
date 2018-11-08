ef7543b87c38558c63093ee1c38804b376ed2f16
sudo1997 <songlinliu1997@gmail.com>
2018-11-08 00:04:29 -0500

    Replace LSTM with LSTMCell, hardcode 2 hidden layer (2 lstmCell), calculate beta only based on hidden state.

--- model.py
+++ model.py
@@ -4,22 +4,39 @@ import torch
 
 
 class stDecoder(nn.Module):
-    def __init__(self, hiddenSize, hiddenLayers, nClasses):
+    def __init__(self, hiddenSize, nLayers, nClasses):
         super(stDecoder, self).__init__()
         self.hiddenSize=hiddenSize
-        self.hiddenLayers=hiddenLayers
+        self.nLayers=nLayers
         self.nClasses=nClasses
-        self.sAtten=nn.Linear((self.hiddenSize*self.hiddenLayers+512),1)
-        self.rnn=nn.LSTM(512,self.hiddenSize)
-        
+        self.initc=nn.Linear(512,self.hiddenSize)
+        self.inith=nn.Linear(512,self.hiddenSize)
+        self.sAtten=nn.Linear(self.hiddenSize*self.nLayers+512, 1)
+        self.tAtten=nn.Linear(self.hiddenSize*self.nLayers, 1)
+        self.lstm1=nn.LSTMCell(512, self.hiddenSize)
+        self.lstm2=nn.LSTMCell(self.hiddenSize,self.hiddenSize)
+        self.tanh=nn.Tanh()
+        self.relu=nn.ReLU()
+        self.fc=nn.Linear(self.hiddenSize,self.nClasses)
 
-    def forward(self, x, hidden):
+    def initHidden(self,x):
+        # x is the first flattend feature rectangel of a video
+        x=torch.mean(x,dim=1,keepdim=True) #(512, 1)
+        x=torch.t(x)                       #(1, 512)
+        c0=self.initc(x)                   #(1, hiddenSize)
+        h0=self.inith(x)                   #(1, hiddenSize)
+        return h0,c0
+
+
+    def forward(self, x, hc1, hc2):
         # x: (channels * pixels). (512,196)
-        # hidden: (num_layers, batch, hidden_size)
+        # hc1 is a (h,c) of the 1st hidden layer of LSTM
+        # h is hideen state, c is cell state
 
-        hidden=torch.squeeze(hidden,1) #(num_layers , hidden_size)
+        hidden=torch.cat((hc1[0],hc2[0]),dim=1) #(1, 2*hiddenSize)
 
-        hidden=hidden.view(-1)
+        hidden=hidden.view(-1)  # flatten hidden 
+
 
         expandedHidden=torch.stack([hidden]*196, dim=0) #(196, num_layers* hidden_size)
 
@@ -33,17 +50,36 @@ class stDecoder(nn.Module):
 
         Y=torch.sum(torch.t(attn_applied),dim=1) #(512,)
 
-        Y=torch.unsqueeze(Y,0)
-        Y=torch.unsqueeze(Y,0)  #(1,1,512)
+        Y=torch.unsqueeze(Y,0) #(1,512)
         
-        print(Y.size())
-        # self.rnn()
+        hc1=self.lstm1(Y,hc1)
+        hc2=self.lstm2(hc1[0], hc2)
+
+        output=self.tanh(hc2[0])
+
+        output=self.fc(output)
+
+        hidden=torch.cat((hc1[0],hc2[0]),dim=1)
+        beta=self.relu( self.tAtten(hidden))
+
+        return output, hc1, hc2, beta
+
+
+
 
 
 
 ###############
 myDecoder=stDecoder(256,2,10)
-x=torch.randn(512,196)
-hidden=torch.randn(2,256)
 
-myDecoder(x,hidden)
+x=torch.randn(512,196)
+oneVideo=[x]*10
+
+hc1=myDecoder.initHidden(x)
+hc2=hc1
+
+outputs=[]
+beta=[]
+
+for i, x in enumerate(oneVideo):
+    outputs[i], hc1, hc2, beta[i]=myDecoder(x, hc1, hc2)
