5adb4de01f6d9d36550fb0d48d6a2c2ae72665e2
Ubuntu <ubuntu@ip-172-31-20-144.us-west-2.compute.internal>
2018-12-02 03:20:47 +0000

    inconsistent indent

--- songsongTrain.py
+++ songsongTrain.py
@@ -12,9 +12,8 @@ def main():
 	myDecoder=stDecoder(256,2,51)
 	myLoss = stAttentionLoss(0.1, 0.01)
 	optimizer=torch.optim.Adam(myDecoder.parameters(),lr=0.0001)
-	videos=h5py.File("train.hdf5",'r')['train_set']
-	labels=h5py.File("../train_label.hdf5", 'r')['train_label']
-	pdb.set_trace()
+	videos=h5py.File("train_feat.hdf5",'r')['train_feat']
+	labels=h5py.File("train_label.hdf5", 'r')['train_label']
 	# print(labels.size())
 	for i,(video,label) in enumerate(zip(videos,labels)):
 		optimizer.zero_grad()
@@ -24,6 +23,8 @@ def main():
 		loss = myLoss(logit, label, alphas, betas)
 		loss.backward()
 		optimizer.step()
+                if (i%20==0):
+                    print("video %d loss: %f" % (i, loss.cpu().detach().numpy())
 
 
 
