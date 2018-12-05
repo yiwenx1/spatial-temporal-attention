132948898e2bcd5aae9af8fdc7bd209e2023b8f0
sudo1997 <songlinliu1997@gmail.com>
2018-12-05 13:15:27 -0500

    Change s&t attetnion

--- attenTrain.py
+++ attenTrain.py
@@ -1,67 +1,78 @@
-from model import stDecoder
-from loss import stAttentionLoss
+import torch.nn as nn
+import random
 import h5py
+import torch.nn.functional as F
 import torch
-import numpy as np
 import pdb
-DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
-DEVICE
+from model import stDecoder
+import time
 
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 
 def main():
-    myDecoder=stDecoder(256,2,51)
-    myDecoder=myDecoder.to(DEVICE)
+    trainFeatures=h5py.File("./data/train_feat.hdf5",'r')["train_feat"]
+    trainLabels=h5py.File("./data/train_label.hdf5", 'r')['train_label']
+    print("Loading is done")
+    print("trainFeatures.shape", trainFeatures.shape)
+    print("trainLabels.shape", trainLabels.shape)
 
-    print(myDecoder)
-    myLoss = stAttentionLoss(0.1, 0.01)
-    myLoss = myLoss.to(DEVICE)
+    model=stDecoder(64, 512, 51)
+    model=model.to(DEVICE)
 
-    optimizer=torch.optim.Adam(myDecoder.parameters(),lr=0.001)
-    videos=h5py.File("/home/ubuntu/train_feat.hdf5",'r')['train_feat']
-    labels=h5py.File("/home/ubuntu/train_label.hdf5", 'r')['train_label']
-    # print(labels.size())
-    for epoch in epochs:
+    criterion = stAttentionLoss(0.1, 0.01)
+    criterion=criterion.to(DEVICE)
 
+    optimizer=torch.optim.Adam(model.parameters())
+
+    batchSize=64
+    indexList=list(range(trainFeatures.shape[0]))
+    batches=trainFeatures.shape[0]//batchSize
+    epochs=20
+    batchID=0
+
+    for epoch in range(epochs):
+
+        random.shuffle(indexList)
         begin=time.time()
 
-        for i,(video,label) in enumerate(zip(videos,labels)):
+        for j in range(batches):
+
             optimizer.zero_grad()
 
-            video=torch.from_numpy(video)
-            video=video.to(DEVICE)
+            videos=torch.from_numpy(trainFeatures[(j*batchSize):(j+1)*batchSize])
+            videos=videos.to(DEVICE)
 
-            label=torch.tensor( np.array(label) ).long().unsqueeze(0)
-            label=label.to(DEVICE)
+            labels=torch.from_numpy(trainLabels[(j*batchSize):(j+1)*batchSize]).long()
+            labels=labels.to(DEVICE)
 
-            logits, alphas, betas=myDecoder(video)
+            logits=model(videos)
+
             loss = myLoss(logits, label, alphas, betas)
+
             loss.backward()
+
             optimizer.step()
-            if (i%20==0):
-                print("video %d loss: %f" % (i, loss.cpu().detach().numpy()) )
+
+            batchID+=1
+            if batchID%20==0 :
+                print("batch %d loss is %f" %(batchID, loss.cpu().detach().numpy()))
+                train_prediction = logits.cpu().detach().argmax(dim=1)
+                train_accuracy = (train_prediction.numpy()==labels.cpu().numpy()).mean()
+                print("train_accracy is %f" % train_accuracy)
 
         end=time.time()
 
         print("Epoch %d training time: %.2fs" %(epoch,(end-begin)) )
-        fileName="params"+str(epoch)+".t7"                                                                         
-        torch.save({'enParams':encoder.state_dict(), 'deParams':decoder.state_dict()},fileName)
+        fileName="params"+str(epoch)+".t7"
+        torch.save(model.state_dict(),fileName)
         print("%s is saved." % fileName)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-if __name__== "__main__":
+if __name__ == '__main__':
     main()
+
+
+
+
+
+
+
--- model.py
+++ model.py
@@ -5,23 +5,29 @@ import pdb
 
 
 class stDecoder(nn.Module):
-    def __init__(self, hiddenSize, nLayers, nClasses):
+    def __init__(self, batchSize, hiddenSize, nClasses):
         super(stDecoder, self).__init__()
+        self.batchSize=batchSize
         self.hiddenSize=hiddenSize
-        self.nLayers=nLayers
         self.nClasses=nClasses
-        # self.initc=nn.Linear(512,self.hiddenSize)
-        # self.inith=nn.Linear(512,self.hiddenSize)
-        self.h1=nn.Parameter(torch.randn(1, self.hiddenSize))
-        self.c1=nn.Parameter(torch.randn(1, self.hiddenSize))
-        self.h2=nn.Parameter(torch.randn(1, self.hiddenSize))
-        self.c2=nn.Parameter(torch.randn(1, self.hiddenSize))
+        self.pixels=196
+        self.channels=512
+        self.h1=nn.Parameter(torch.randn(self.batchSize, self.hiddenSize))
+        self.c1=nn.Parameter(torch.randn(self.batchSize, self.hiddenSize))
+        self.h2=nn.Parameter(torch.randn(self.batchSize, self.hiddenSize))
+        self.c2=nn.Parameter(torch.randn(self.batchSize, self.hiddenSize))
 
-        self.sAtten=nn.Linear(self.hiddenSize+512, 1)
-        self.tAtten=nn.Linear(self.hiddenSize, 1)
+        self.spatialBias=nn.Parameter(torch.randn(self.batchSize, self.pixels))
+        self.temporalBias=nn.Parameter(torch.randn(self.batchSize, 1))
+
+        self.h2p=nn.Linear(self.channels, self.pixels)
+        self.c2p=nn.Linear(self.channels, self.pixels)
+
+        self.h21=nn.Linear(self.hiddenSize,1)
+        self.c21=nn.Linear(self.channels,1)
+
         self.lstm1=nn.LSTMCell(512, self.hiddenSize)
         self.lstm2=nn.LSTMCell(self.hiddenSize,self.hiddenSize)
-        self.tanh=nn.Tanh()
         self.relu=nn.ReLU()
         self.fc=nn.Linear(self.hiddenSize,self.nClasses)
 
@@ -36,10 +42,9 @@ class stDecoder(nn.Module):
     #     return h0,c0
 
 
-    def forward(self, video):
-        # x: (frames, channels * pixels). (512,196)
-        # hc1 is a (h,c) of the 1st hidden layer of LSTM
-        # h is hideen state, c is cell state
+    def forward(self, videos):
+        # videos: (N, frames, channels, pixels)
+        videos=videos.permute(1,0,2,3) #(frames, N, channels, pixels)
 
         h1=self.h1
         c1=self.c1
@@ -54,36 +59,38 @@ class stDecoder(nn.Module):
         alphas=list()
 
         for step in range(video.shape[0]):
-            hidden=hc2[0]
-            x=torch.t(video[step]) # 196*512
+            hidden=hc2[0] #(N, hiddenSize)
 
-            beta=self.tAtten( hidden )  #(batchSize, 1)
-            betas.append(beta.squeeze(0))
+            x=videos[step].squeeze(0) #(N, channels, pixels)
 
-            hidden=hidden.view(-1)  # flatten hidden
+            x=x.permute(0, 2, 1) #(N, pixels, channels)
 
-            expandedHidden=torch.stack([hidden]*196, dim=0) #(196, hidden_size)
+            sAtten1=self.h2p(hidden) #(N, pixels)  spatial attention term1
+            sAtten2=self.c2p(x).squeeze(2) #(N, pixels) spatial attention term2
 
-            sAttenInput=torch.cat((expandedHidden, x), dim=1)  # 196 x (hidden_size +512)
+            energy=torch.add(torch.add(sAtten1, sAtten2), self.spatialBias) #(batchSize, pixels)
 
-            sAttenInput=sAttenInput.unsqueeze(0) #(batchSize, 196, hidden_size+512)
+            energy=F.softmax(energy,dim=1) #(N, pixels)
 
-            energy=self.sAtten(sAttenInput).squeeze(0)                   # (196, 1)
+            energy=F.normalize(energy,dim=1, p=1)
 
-            attn_weights=F.softmax(energy,dim=0)            # (196, 1)
+            alphas.append(energy)
 
-            alphas.append(attn_weights)
+            energy=energy.unsqueeze(1) #(N, 1, pixels)
 
-            attn_applied=torch.mul(attn_weights, x) # (196,512)
+            attn_applied=torch.mul(energy, x) # (N, 1, channels)
 
-            Y=torch.sum(torch.t(attn_applied),dim=1) #(512,)
+            Y=attn_applied.squeeze(1) #(N, channels)
 
-            Y=torch.unsqueeze(Y,0) #(batchSize,512)
+            beta=self.h21(hidden).squeeze(0)+self.c21(Y).squeeze(0)+self.temporalBias
+            beta=self.relu(beta)
+            betas.append(beta)
+
             hc1=self.lstm1(Y,hc1)
             hc2=self.lstm2(hc1[0], hc2)
 
-            output=self.tanh(hc2[0])
-            outputs.append(self.fc(output))
+            output=self.fc(hc2[0]) #(N, nClasses)
+            outputs.append(output)
 
         # pdb.set_trace()
         alphasTensor=torch.stack(alphas, dim=0)
@@ -91,7 +98,7 @@ class stDecoder(nn.Module):
         outputsTensor=torch.stack(outputs,dim=0) #(seq_length, 1, nClasses)
         outputsTensor=outputsTensor.squeeze(1)
 
-        logits=torch.mul(betasTensor, outputsTensor)      #(seq_length, nClasses)
+        logits=torch.bmm(betasTensor, outputsTensor)      #(seq_length, nClasses)
         logits=torch.sum(logits, dim=0, keepdim=True)
 
         return logits, alphasTensor, betasTensor
