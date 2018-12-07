6ef33d6e23256439ba4cd26dd7a5e13363d3a958
Sirius Zhang <sirius@SiriusdeMBP.wv.cc.cmu.edu>
2018-12-07 09:04:11 +0800

    switch loss to batch processing

--- loss.py
+++ loss.py
@@ -23,22 +23,25 @@ class stAttentionLoss(nn.Module):
     Implement the loss function
     @param 
     
-    inputs: model output # dimension: # (seq_length, nClasses)
+    inputs: model output # dimension: # (batch_size, seq_length, nClasses)
     target: true label # dimension: # (1) #### for one video, LongTensor scalar; for batch, (batch_size, 1)
-    alpha: attn_weights # dimension: # (seq_length, 196, 1)
-    beta: betas returned by model #(seq_length, 1)
+    alpha: attn_weights # dimension: # (batch_size, seq_length, 196)
+    beta: betas returned by model #(batch_size, seq_length, 1)
 
     @return: loss Tensor scalar
     """
     def forward(self, inputs, target, alpha, beta):
-        alpha = torch.squeeze(alpha,dim = 2)
-        penalty_1 = self.lambda_1 * torch.sum(1 - torch.sum(alpha,dim = 0), dim = 0)
+        #alpha = torch.squeeze(alpha,dim = 2)
+        penalty_1 = self.lambda_1 * torch.sum(1 - torch.sum(alpha,dim = 1), dim = 1)
         
-        beta_norm = torch.abs(beta) # Here should be norm |beta_{t}| #(seq_length, 1)
-        penalty_2 = self.lambda_1 * torch.sum(beta_norm) # Here should be summing over dim=0
+        beta_norm = torch.abs(beta) # Here should be norm |beta_{t}| #(batch_size, seq_length)
+        penalty_2 = self.lambda_1 * torch.sum(beta_norm,dim = 1) # Here should be summing over dim=0
+
+        penalty_2 = torch.squeeze(penalty_2,dim = 1)
+
 
         # using cross entropy means have softmax inside loss, DO NOT add softmax at the end of model?
-        return F.cross_entropy(inputs,target) + penalty_1 + penalty_2
+        return F.cross_entropy(inputs,target) + torch.mean(penalty_1) + torch.mean(penalty_2)
 
 ###################
 
@@ -49,14 +52,15 @@ class stAttentionLoss(nn.Module):
 
 # label = torch.LongTensor([0])
 
-# attn_weights = np.zeros((20,196))
-# for i in range(20):
-#     attn_weights[i] = np.array([1 / 196] * 196)
+# attn_weights = np.zeros((2,20,196))
+# for j in range(2):
+#     for i in range(20):
+#         attn_weights[j][i] = np.array([1 / 196] * 196)
 # attn_weights = torch.Tensor(attn_weights)
-# attn_weights = torch.unsqueeze(attn_weights,dim = 2)
+# #attn_weights = torch.unsqueeze(attn_weights,dim = 0)
 # print(attn_weights.size())
 
-# betas = torch.Tensor([[0.0501],
+# betas = torch.Tensor([[[0.0501],
 #         [0.0285],
 #         [0.0274],
 #         [0.0275],
@@ -75,9 +79,31 @@ class stAttentionLoss(nn.Module):
 #         [0.0286],
 #         [0.0286],
 #         [0.0286],
-#         [0.0286]])
+#         [0.0286]],[[0.0501],
+#         [0.0285],
+#         [0.0274],
+#         [0.0275],
+#         [0.0278],
+#         [0.0280],
+#         [0.0281],
+#         [0.0282],
+#         [0.0283],
+#         [0.0284],
+#         [0.0284],
+#         [0.0285],
+#         [0.0285],
+#         [0.0285],
+#         [0.0286],
+#         [0.0286],
+#         [0.0286],
+#         [0.0286],
+#         [0.0286],
+#         [0.0286]]])
 # print(betas.size())
-# ##########################################
+
+
+###########################################
 
 # myLoss = stAttentionLoss(0.1, 0.01)
 # loss = myLoss(input, label, attn_weights, betas)
+# print(loss)
